de87a1ef74599a496bb6a9984c73c03af5d57d1e
Nikita <Mandalika>
2023-06-28 10:42:49 -0400

    Initial Commit

---
+++ kwkCoreData/kwkCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  kwkCoreDataApp.swift
+//  kwkCoreData
+//
+//  Created by Nikita Mandalika on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct kwkCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
